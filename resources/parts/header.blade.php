90e5cf24d1bfece19bab776942f764502db2c88f
Sergio <sergio.marretta@libero.it>
2021-05-10 16:42:11 +0200

    broken

---
+++ resources/parts/header.blade.php
@@ -0,0 +1,12 @@
+   <header>
+        <div class="wrapper center">
+            <img class="logo-brand" src="{{asset('images/marchio-sito-test.png')}}" alt="Molisana Logo">
+          <div class="nav">
+              <ul>
+                  <li><a href="">Home</a></li>
+                  <li><a href="">Prodotti</a></li>
+                  <li><a href="">News</a></li>
+              </ul>
+          </div>
+        </div>
+    </header>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,19 +10,10 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    <header>
-        <div class="wrapper center">
-            <img class="logo-brand" src="{{asset('images/marchio-sito-test.png')}}" alt="Molisana Logo">
-          <div class="nav">
-              <ul>
-                  <li><a href="">Home</a></li>
-                  <li><a href="">Prodotti</a></li>
-                  <li><a href="">News</a></li>
-              </ul>
-          </div>
-        </div>
-    </header>
-    <main>
+    
+ @include('parts.header')
+    
+ <main>
 <div class="l_pasta wrapper">
     <h3>LE LUNGHE</h3>
 </div>
@@ -69,56 +60,9 @@
       
 
     </main>
-    <footer class="wrapper my_flex">
-        <div class="f-left">
-            <img class="logo-brand" src="{{asset('images/marchio-sito-test.png')}}" alt="Molisana Logo">
-            <ul>
-                <li>Ragione Sociale: La Molisana S.P.A.</li>
-                <li>Sede legale: Contrada Colle delle Alpi, 100/A</li>
-                <li>86100 - Campobasso (CB)</li>
-                <li>Pec: lamolisana@pec.it</li>
-                <li>Tel: +39 0874 4981</li>
-                <li>Fax: +39 0874 629584</li>
-                <li>info@lamolisana.it(per segnalazioni degli utenti)</li>
-                <li>commerciale@lamolisana.it</li>
-                <li>export@lamolisana.it</li>
-                <li>numero verde 800818081</li>
-                <li>telefono 3801292455</li>
-                </ul>       
-        </div>
-        <div class="f-center">
-            <ul>
-               <li>PASTIFICIO</li>
-               <li>Il Pastificio</li>
-               <li>Grano decorticato a pietra</li>
-               <li>Il Molise c'è</li>
-               <li>Filiera integrata</li>
-               <li>100 anni di pasta</li>
-               <li>Sartoria della pasta</li>
-               <li>Spaghetto Quadrato</li>
-               <li>Le Persone</li>
-               <li>PRODOTTI</li>
-               <li>Le Classiche </li>
-               <li>Le integrali</li>
-               <li>Le Biologiche</li>
-               <li>Le Gluten-Free</li>
-               <li>Le Semole</li>
-               <li>Le Extra di Lusso</li>
-               
-            </ul>
 
-        </div>
-            <div class="f-right">
-                <ul>
-                    <li>COLLEZIONE DA CHEF</li>
-                    <li>Collezione da Chef</li>
-                    <li>Grandi Cucine</li>
-                    <li>Biologiche</li>
-                    <li>Quadrate</li>
-                </ul>
+ @include('parts.footer')
 
-            </div>
-
-    </footer>
+    
 </body>
 </html>
