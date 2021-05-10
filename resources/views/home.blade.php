5858d2c9d0cc475c0f55f419a06698f0b7de019e
Sergio <sergio.marretta@libero.it>
2021-05-10 02:22:22 +0200

    part2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
     </header>
     <main>
 <div class="l_pasta wrapper">
-    <h3>Le lunghe</h3>
+    <h3>LE LUNGHE</h3>
 </div>
         <div class="long my_flex wrapper">
           
@@ -38,7 +38,7 @@
             
         </div>
         <div class="md_pasta wrapper">
-    <h3>Le corte</h3>
+    <h3>LE CORTE</h3>
 </div>
          <div class="medium my_flex wrapper">
           
@@ -52,7 +52,7 @@
             
         </div>
         <div class="s_pasta wrapper">
-    <h3>Le cortissime</h3>
+    <h3>LE CORTISSIME</h3>
 </div>
          <div class="short my_flex wrapper">
           
@@ -69,7 +69,55 @@
       
 
     </main>
-    <footer>
+    <footer class="wrapper my_flex">
+        <div class="f-left">
+            <img class="logo-brand" src="{{asset('images/marchio-sito-test.png')}}" alt="Molisana Logo">
+            <ul>
+                <li>Ragione Sociale: La Molisana S.P.A.</li>
+                <li>Sede legale: Contrada Colle delle Alpi, 100/A</li>
+                <li>86100 - Campobasso (CB)</li>
+                <li>Pec: lamolisana@pec.it</li>
+                <li>Tel: +39 0874 4981</li>
+                <li>Fax: +39 0874 629584</li>
+                <li>info@lamolisana.it(per segnalazioni degli utenti)</li>
+                <li>commerciale@lamolisana.it</li>
+                <li>export@lamolisana.it</li>
+                <li>numero verde 800818081</li>
+                <li>telefono 3801292455</li>
+                </ul>       
+        </div>
+        <div class="f-center">
+            <ul>
+               <li>PASTIFICIO</li>
+               <li>Il Pastificio</li>
+               <li>Grano decorticato a pietra</li>
+               <li>Il Molise c'è</li>
+               <li>Filiera integrata</li>
+               <li>100 anni di pasta</li>
+               <li>Sartoria della pasta</li>
+               <li>Spaghetto Quadrato</li>
+               <li>Le Persone</li>
+               <li>PRODOTTI</li>
+               <li>Le Classiche </li>
+               <li>Le integrali</li>
+               <li>Le Biologiche</li>
+               <li>Le Gluten-Free</li>
+               <li>Le Semole</li>
+               <li>Le Extra di Lusso</li>
+               
+            </ul>
+
+        </div>
+            <div class="f-right">
+                <ul>
+                    <li>COLLEZIONE DA CHEF</li>
+                    <li>Collezione da Chef</li>
+                    <li>Grandi Cucine</li>
+                    <li>Biologiche</li>
+                    <li>Quadrate</li>
+                </ul>
+
+            </div>
 
     </footer>
 </body>
